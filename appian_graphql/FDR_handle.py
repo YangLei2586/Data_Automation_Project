d86061ba51ebc59c5bce41ddc8212a9199706131
YangLei2586 <ylei2@pride.hofstra.edu>
2022-08-29 13:16:04 -0700

    completed FDR handle class

--- appian_graphql/FDR_handle.py
+++ appian_graphql/FDR_handle.py
@@ -49,21 +49,208 @@ class FDRHandle(APIEndPint):
             json_headers: dict,
             graph_statement: str
     ) -> list:
+        """
+        Prepares FDR graph ql statement list
+        :param json_headers: HTTP headers passed via Helios call
+        :param graph_statement: function to get the latest graph statement
+        :return:
+        """
+        graph_ql = graph_statement
+        for http_key, stmnt_key in self._config.get_http_arg_map_key_pairs():
 
-    def pivot_function(self,p_json_data: dict, metadata:dict=None) -> dict:
+            # if http_key has been sent via Helios call
+            # replace the stmnt_key with the received data
+            if http_key in json_headers:
+                graph_ql = graph_ql.replace(
+                    stmnt_key, list_to_string(json_headers[http_key])
+                )
+            # if http_key has not been sent via Helios call
+            # check default fall back parameters
+            elif self._config.is_stmt_in_fallback_params(stmnt_key):
+                graph_ql = graph_ql.replace(
+                    stmnt_key,
+                    list_to_string(
+                        self._config.get_fallback_graph_key(stmnt_key)
+                    )
+                )
+
+        # nickname and templates
+        nick_and_temps, UNIQUE_TEMPLATE_SECTORS = unwrap_nick_temps_and_sectors(
+            json_headers[_HELIOS_NICKNAME_TEMPLATE_SECTOR_TUPLE_KEY]
+        )
+        statement_list = []
+        for template_id, sector_id in UNIQUE_TEMPLATE_SECTORS:
+            nicknames_matching_template = [
+                __[0] for __ in nick_and_temps
+                if __[1] == template_id and __[2] == sector_id
+
+            ]
+            fdrs = self._FDR_MAP.get_fdr(
+                template_id,
+                sector_id
+            )
+            query_statement = (
+                graph_ql.replace(
+                    _APPIAN_TEMPLATE_NAME_KEY,
+                    self._FDR_MAP.get_template_name(template_id, sector_id)
+                ).replace(
+                    _APPIAN_NICKNAME_LIST_KEY,
+                    list_to_string(nicknames_matching_template)
+                ).replace(
+                    _APPIAN_FDR_ID_LIST_KEY,
+                    list_to_string(fdrs)
+                )
+            )
+            pivot_params = {
+                'template_id': template_id,
+                'sector_id': sector_id
+
+            }
+            unpivot_params = {
+                **pivot_params,
+                **{'nickname_ids': nicknames_matching_template}
+            }
+            statement_list.append(
+                APIInputParams(
+                    url=get_graph_ql_web_link(),
+                    request_type='get',
+                    params={'query': query_statement},
+                    pivot_function_params=pivot_params,
+                    unpivot_function_params=unpivot_params
+                )
+            )
+        return statement_list
+
+    def pivot_function(self, p_json_data: dict, metadata: dict = None) -> dict:
         """
         Turns Json response into {{},{},{},{}...}
         :param p_json_data: Json response from the API
         :param metadata: dict
         :return:
         """
-    def pivot_function_with_details(self, p_json_data: dict, metadata: dict = None)->dict:
+        for agent_details in p_json_data['data']['getFDRData']:
+            agent_id = agent_details['agent']['agent_id']
+            nickname_id = agent_details['nicknameId']
+            for agent_data in agent_details['statementMaster']:
+                report_date = agent_data['statementDate']
+                period_type = get_value_from_api_dict(
+                    'periodType', 'periodTypeDesc', agent_data
+                )
+                for datapoints in agent_data['templateStatementInfo']:
+                    # pull datapoints that have been reviewed
+                    if datapoints['analystReviewed'] == 'Y':
+                        for datapoint in datapoints['stmntData']:
+                            try:
+                                # only get datapoints with adjusted value present
+                                if datapoint['adjustedValue'] is not None:
+                                    year_int = int(
+                                        datetime.strftime(
+                                            datetime.strftime(
+                                                report_date,
+                                                '%Y-%m-%d'
+                                            ),
+                                            '%Y'
+                                        )
+                                    )
+                                    data = {
+                                        'agent_id': agent_id,
+                                        'nickname_id': nickname_id,
+                                        'sector_id': metadata['sector_id'],
+                                        'template_id': metadata['template_id'],
+                                        'report_date': year_int,
+                                        'period_type': period_type,
+                                        'fdr_id': datapoint['fdrId'],
+                                        'adjustedVaule': float(
+                                            datapoint['adjustedValue']
+                                        )
+                                    }
+                                    self._OFC.add_data(report_date, period_type, data)
+                            except Exception as e:
+                                print(
+                                    "Error for " + str(agent_id) + "" + str(report_date)
+                                    + "" + str(nickname_id) + "" + str(datapoint['fdrId'])
+                                    + "" + str(datapoint['adjustedValue'])
+                                )
+                                print(str(e))
+        return self._OFC.get_data()
+
+    def pivot_function_with_details(self, p_json_data: dict, metadata: dict = None) -> dict:
         """
         Turns Json response into {{},{},{},{}...}
         :param p_json_data: Json response from the API
         :param metadata: dict
         :return:
         """
+        compare_analyst_flag = True
+        if metadata['template_id'] == '2':
+            compare_analyst_flag = False
+
+        for agent_details in p_json_data['data']['getFDRData']:
+            agent_id = agent_details['agent']['agentId']
+            nickname_id = agent_details['nicknameId']
+            for agent_data in agent_details['statementMaster']:
+                fiscal_end_year = agent_data['fiscalYearEnd']
+                report_date = agent_data['statementDate']
+                exchange_rate = agent_data['exchangeRate']
+                scale_desc = get_value_from_api_dict(
+                    'scale', 'scaleDesc', agent_data
+                )
+                currency_code = get_value_from_api_dict(
+                    'currency', 'currencyCode', agent_data
+                )
+                period_type = get_value_from_api_dict(
+                    'periodType', 'periodTypeDesc', agent_data
+                )
+                statement_type = get_value_from_api_dict(
+                    'statementType', 'statementTypeDesc', agent_data
+                )
+                for datapoints in agent_data['templateStatementInfo']:
+                    # pull datapoints that have been reviewed
+                    if (
+                            (not compare_analyst_flag)
+                            or
+                            (compare_analyst_flag and datapoints['analystReviewed'] == 'Y')
+                    ):
+                        private_flag = (
+                            None if 'privateFlg' not in datapoints
+                            else datapoints['privateFlg']
+                        )
+
+                        for datapoint in datapoints['stmntData']:
+                            try:
+                                if datapoint['adjustedValue'] is not None:
+                                    year_int = int(
+                                        datetime.strftime(
+                                            datetime.strftime(
+                                                report_date,
+                                                '%Y-%m-%d'
+                                            ),
+                                            '%Y'
+                                        )
+                                    )
+                                data = {
+                                    'agent_id': agent_id,
+                                    'nickname_id': nickname_id,
+                                    'sector_id': metadata['sector_id'],
+                                    'template_id': metadata['template_id'],
+                                    'report_date': year_int,
+                                    'period_type': period_type,
+                                    'fdr_id': datapoint['fdrId'],
+                                    'adjustedVaule': float(
+                                        datapoint['adjustedValue']
+                                    )
+                                }
+                                self._OFC.add_data(report_date, period_type, data)
+
+                            except Exception as e:
+                                print(
+                                    "Error for " + str(agent_id) + "" + str(report_date)
+                                    + "" + str(nickname_id) + "" + str(datapoint['fdrId'])
+                                    + "" + str(datapoint['adjustedValue'])
+                                )
+                                print(str(e))
+        return self._OFC.get_data()
+
     def get_data(
             self,
             pivot_function,
@@ -75,14 +262,20 @@ class FDRHandle(APIEndPint):
         :param unpivot_function:
         :return:
         """
+        self._FDRData = self.get_appian_data(
+            self._FDRStatements,
+            pivot_function=pivot_function,
+            unpivot_function=unpivot_function
+        )
+
     def print_data(
             self,
-            mode = 'default',
-            destination = sys.stdout,
-            header = False,
-            newline = '\n'
+            mode='default',
+            destination=sys.stdout,
+            header=False,
+            newline='\n'
     ):
-        if mode not in ('default','csv','json','raw'):
+        if mode not in ('default', 'csv', 'json', 'raw'):
             return ValueError(
                 'Only None(i.e. std.output),csv, json, raw are allowed'
             )
@@ -92,8 +285,8 @@ class FDRHandle(APIEndPint):
                     dict_writer = csv.DictWriter(
                         f,
                         self._FDRData[0].keys(),
-                        delimiter = self._config.get_print_delimiter()
-                     )
+                        delimiter=self._config.get_print_delimiter()
+                    )
                     if header:
                         dict_writer = csv.DictWriter()
                     dict_writer.writerows(self._FDRData)
@@ -103,14 +296,14 @@ class FDRHandle(APIEndPint):
                     dict_writer = csv.DictWriter(
                         f,
                         self._FDRData[0].keys(),
-                        delimiter = self._config.get_csv_delimiter()
+                        delimiter=self._config.get_csv_delimiter()
                     )
                     if header:
                         dict_writer.writeheader()
                     dict_writer.writerows(self._FDRData)
 
         elif mode == 'json':
-            if isinstance(destination,str):
+            if isinstance(destination, str):
                 with open(destination, 'w+', newline=newline) as f:
                     f.write(json.dumps(self._FDRData))
             else:
@@ -119,7 +312,3 @@ class FDRHandle(APIEndPint):
 
         elif mode == 'raw':
             return self._FDRData
-        
-
-
-
